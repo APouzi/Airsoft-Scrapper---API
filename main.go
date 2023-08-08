26fea04984407265c8d6af5096b724c3e7b27d21
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-08 06:29:35 +0100

    WIP got some prices going with regex, but it's so messed with the hierarchy -AP

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -16,6 +18,8 @@ func main() {
 
 	htmlParser := html.NewTokenizer(request.Body)
 	var startTokenDiv bool
+	lctCollection := []string{}
+	// checkPrice := `/(?=.)\$(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?/`
 	for {
 		ele := htmlParser.Next()
 
@@ -39,20 +43,28 @@ func main() {
 			elementProd = htmlParser.Token()
 		}
 		if startTokenDiv{
+			if elementProd.Type == html.TextToken{
+				bool, _ := regexp.MatchString(`\$\d+(?:\.\d+)?`, elementProd.String())
+				if bool {
+					fmt.Println(elementProd.Data)
+				}
+				
+			}
 			if len(elementProd.Attr) > 1 {
 				for _, v := range elementProd.Attr{
-					// fmt.Println(v)
 					if v.Key == "title" && v.Val != "Add to Cart"{
-						fmt.Println(v.Val)
+						// fmt.Println(v.Val)
+						if strings.Contains(v.Val, "LCT"){
+							lctCollection = append(lctCollection, v.Val)
+							
+						}
 					}
-					// if v.Key == "title"{
-					// 	fmt.Println(v.Val)
-					// }
 				}
 				
 			}
 		}
 			
 	}
+	fmt.Println(lctCollection)
 	
 }
